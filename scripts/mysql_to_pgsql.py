6768256117b3f49ec72307575c5b551e5ea0f773
Omar Attia <oy.attia@gmail.com>
2020-03-17 09:07:20 -0400

    test mysql

--- scripts/mysql_to_pgsql.py
+++ scripts/mysql_to_pgsql.py
@@ -2,8 +2,22 @@ import mysql.connector
 
 
 def main():
-    cnx = mysql.connector.connect(user='scott', password='password',
+    cnx = mysql.connector.connect(user='root', password='abcd1234',
                                   host='127.0.0.1',
-                                  database='employees')
+                                  database='wiki')
+
+    cursor = cnx.cursor()
+
+    query = f"""
+        select * from wiki.langlinks limit 100
+    """
+
+    cursor.execute(query)
+
+    for p in cursor:
+        print(p)
+
+    cursor.close()
+
     cnx.close()
 
